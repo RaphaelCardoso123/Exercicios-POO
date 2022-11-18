49bcd138c837ec45ad9658b66bfa378388f617e3
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-11-18 11:49:09 -0300

    ...

---
+++ SegundoProjeto/SegundoProjeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando");
+            Console.ReadLine();
+        }
+    }
+}
