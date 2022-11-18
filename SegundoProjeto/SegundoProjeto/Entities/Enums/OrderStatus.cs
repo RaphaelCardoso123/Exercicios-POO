12f27d5249a98a829c3090c37443f6534c207a68
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-11-18 12:00:22 -0300

    ...

---
+++ SegundoProjeto/SegundoProjeto/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SegundoProjeto.Entities.Enums
+
+namespace SegundoProjeto.Entities.Enums
+{
+    class OrderStatus : Int
+    {
+
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+
+    }
+}
---
+++ SegundoProjeto/SegundoProjeto/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundoProjeto.Entities
+{
+    class Order
+    {
+
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Order { get; set; }
+
+    }
+}
