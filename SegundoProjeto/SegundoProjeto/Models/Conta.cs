c0ddf8835700f5d36140e3986a298c2556992339
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2022-11-20 14:17:51 -0300

    ...

---
+++ SegundoProjeto/SegundoProjeto/Models/Conta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundoProjeto.Models
+{
+    class Conta
+    {
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+        public int Agencia { get; set; }
+        public double Saldo { get; set; }
+
+        //Método (Ação)
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void ExibirResumoConta()
+        {
+            Console.WriteLine($"Correntista: {Nome}.");
+            Console.WriteLine($"Número: {Numero}.");
+            Console.WriteLine($"Agencia: {Agencia}.");
+            Console.WriteLine($"Saldo: {Saldo}.");
+        }
+    }
+}
--- SegundoProjeto/SegundoProjeto/Models/ContaPoupanca.cs
+++ SegundoProjeto/SegundoProjeto/Models/ContaPoupanca.cs
@@ -4,28 +4,8 @@ using System.Text;
 
 namespace SegundoProjeto.Models
 {
-    class ContaPoupanca
+    class ContaPoupanca : Conta
     {
-
-        public string Nome { get; set; }
-        public int Numero { get; set; }
-        public int Agencia { get; set; }
-        public double Saldo { get; set; }
         public double Juros { get; set; }
-
-        //Método (Ação)
-        public void Depositar (double valor)
-        {
-            Saldo += valor;
-        }
-
-        public void ExibirResumoConta()
-        {
-            Console.WriteLine($"Correntista: {Nome}.");
-            Console.WriteLine($"Número: {Numero}.");
-            Console.WriteLine($"Agencia: {Agencia}.");
-            Console.WriteLine($"Saldo: {Saldo}.");
-        }
-
     }
 }
