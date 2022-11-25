599b8f0d668040d072a37619a0055d038f68d852
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-11-25 10:40:50 -0300

    ...

--- SegundoProjeto/SegundoProjeto/Program.cs
+++ SegundoProjeto/SegundoProjeto/Program.cs
@@ -11,32 +11,28 @@ namespace SegundoProjeto
     {
         static void Main(string[] args)
         {
-
             
             Livro livro = new Livro();
-            livro.NomeLivro = "Teste";
-
+            livro.NomeLivro = "The catcher in the Rye";
             Livro livro2 = new Livro();
-            livro2.NomeLivro = "Testando";
-
+            livro2.NomeLivro = "Rich Dad Poor Dad";
             Livro livro3 = new Livro();
-            livro3.NomeLivro = "Testado";
-
-
+            livro3.NomeLivro = "Richest Man in Babylon";
+            
 
             Autor autor1 = new Autor();
-            autor1.Nome = "The catcher in the Rye";
+            autor1.Nome = "J. D. Salinger";
             autor1.Email = "salinger@gmail.com";
 
             Autor autor2 = new Autor();
-            autor2.Nome = "Rich Dad Poor Dad";
+            autor2.Nome = "Robert Kiyosaki";
             autor2.Email = "robert@hotmail.com";
 
             Autor autor3 = new Autor();
-            autor3.Nome = "Richest Man in Babylon";
+            autor3.Nome = "George Samuel Clason ";
             autor3.Email = "george@gmail.com";
-
             
+
             livro.Autores.Add(autor1);
             livro2.Autores.Add(autor2);
             livro3.Autores.Add(autor3);
@@ -48,7 +44,7 @@ namespace SegundoProjeto
                 Console.WriteLine(livro.NomeLivro);
                 foreach (Autor obj in livro.Autores)
                 {
-                    Console.WriteLine($"Nome: {obj.Nome}.");
+                    Console.WriteLine($"Autor: {obj.Nome}.");
                     Console.WriteLine($"Email: {obj.Email}.");
                     Console.WriteLine("");
                 }
@@ -56,121 +52,20 @@ namespace SegundoProjeto
                 Console.WriteLine(livro2.NomeLivro);
                 foreach (Autor obj in livro2.Autores)
                 {
-                    Console.WriteLine($"Nome: {obj.Nome}.");
+                    Console.WriteLine($"Autor: {obj.Nome}.");
                     Console.WriteLine($"Email: {obj.Email}.");
                     Console.WriteLine("");
                 }
 
                 Console.WriteLine(livro3.NomeLivro);
-
                 foreach (Autor obj in livro3.Autores)
                 {
-                    Console.WriteLine($"Nome: {obj.Nome}.");
+                    Console.WriteLine($"Autor: {obj.Nome}.");
                     Console.WriteLine($"Email: {obj.Email}.");
                     Console.WriteLine("");
                 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*
-            List<Autor> Autores = new List<Autor>();
             
-
-            Autor autor1 = new Autor( ) { Nome = "The catcher in the Rye", Email = "salinger@gmail.com" };
-            Autores.Add(autor1);
-          
-            Autor autor2 = new Autor() { Nome = "Rich Dad Poor Dad", Email = "robert@hotmail.com" };
-            Autores.Add(autor2);
-
-            Autor autor3 = new Autor() { Nome = "Richest Man in Babylon", Email = "george@gmail.com" };
-            Autores.Add(autor3);
-            
-
-            Console.WriteLine("Livros:");
-            Console.WriteLine("");
-
-            foreach (Autor obj in Autores)
-            {
-                Console.WriteLine($"Nome: {obj.Nome}.");
-                Console.WriteLine($"Email: {obj.Email}.");
-                Console.WriteLine("");
-            }
-            */
-
-
-
-
-
-
-
-
-
-
-
-
-            /*
-            List<Autor> Autores = new List<Autor>();
-            
-            
-
-            Autor autor1 = new Autor();
-            autor1.Nome_autor = "The catcher in the Rye";
-            autor1.Email = "salinger@gmail.com";
-            
-            Autor autor2 = new Autor();
-            autor2.Nome_autor = "Rich Dad Poor Dad";
-            autor2.Email = "robert@hotmail.com";
-         
-            Autor autor3 = new Autor();
-            autor3.Nome_autor = "Richest Man in Babylon";
-            autor3.Email = "george@gmail.com";
-
-
-            Autores.Add(autor1);
-            Autores.Add(autor2);
-            Autores.Add(autor3);
-
-            Console.WriteLine("Livros:");
-            Console.WriteLine("");
-
-            foreach (Autor obj in Autores)
-            {
-                Console.WriteLine($"Nome: {obj.Nome_autor}.");
-                Console.WriteLine($"Email: {obj.Email}.");
-                Console.WriteLine("");
-            }
-             */
-
 
 
 
